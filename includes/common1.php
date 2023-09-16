1ec8245d1fbd153d5d1387105f2a878603e4dca4
premsharma2206 <premsharma2206@gmail.com>
2023-09-16 17:19:53 -0500

    Changed db credentials in another file

--- includes/common1.php
+++ includes/common1.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "store")or die($mysqli_error($con));
+$con = mysqli_connect("localhost", "u649539383_premsharma", "Prem@1014219", "u649539383_bhawani")or die($mysqli_error($con));
 ?>
