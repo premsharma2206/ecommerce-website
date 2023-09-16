f2c0f133cde580bd9be17c48b7c1d88559934ea5
premsharma2206 <premsharma2206@gmail.com>
2023-09-16 18:04:20 -0500

    Updated footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
         <center>
-            <p>Copyright &copy; First Choice. All Rights Reserved  |  Contact Us: +1 000000000</p>	
+            <p>Copyright &copy; Bhawani Emporium. All Rights Reserved  |  Contact Us: +1 000000000</p>
         </center>
     </div>
 </footer>
