3fefcf5abe7cefb93579372efdfdf09a1e84833f
premsharma2206 <premsharma2206@gmail.com>
2023-09-17 10:29:02 -0500

    change contact number on footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
         <center>
-            <p>Copyright &copy; Bhawani Emporium. All Rights Reserved  |  Contact Us: +1 000000000</p>
+            <p>Copyright &copy; Bhawani Emporium. All Rights Reserved  |  Contact Us: +91 9966669702</p>
         </center>
     </div>
 </footer>
