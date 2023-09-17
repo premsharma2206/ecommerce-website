2ca87d889ea0d62d03ca4b688f317cacd4dccdd1
premsharma2206 <premsharma2206@gmail.com>
2023-09-17 14:37:42 -0500

    Added php mailer

---
+++ phpmailer.php
@@ -0,0 +1,24 @@
+<?php
+   require 'vendor/autoload.php';
+   use PHPMailer\PHPMailer\PHPMailer;
+   $mail = new PHPMailer;
+   $mail->isSMTP();
+   $mail->SMTPDebug = 2;
+   $mail->Host = 'smtp.hostinger.com';
+   $mail->Port = 587;
+   $mail->SMTPAuth = true;
+   $mail->Username = 'premsharma@bhawaniemporium.com';
+   $mail->Password = 'Prem@1014219';
+   $mail->setFrom('premsharma@bhawaniemporium.com', 'Prem Sharma');
+   $mail->addReplyTo('premsharma@bhawaniemporium.com', 'Prem Sharma');
+   $mail->addAddress('premsharma0699@gmail.com', 'Sharma');
+   $mail->Subject = 'Checking if PHPMailer works';
+   $mail->msgHTML(file_get_contents('message.html'), __DIR__);
+   $mail->Body = 'This is just a plain text message body';
+   //$mail->addAttachment('attachment.txt');
+   if (!$mail->send()) {
+       echo 'Mailer Error: ' . $mail->ErrorInfo;
+   } else {
+       echo 'The email message was sent.';
+   }
+?>
